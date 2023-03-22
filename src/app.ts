0cce5ec5c47cc62fdab3e594941e46795218750c
Eugene Odeluga <eugene.odeluga@agent3.com>
2023-03-22 06:55:11 +0000

    Setting up subscription handlers

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,52 @@
 import { WebSocket } from "ws";
 
+type WebSocketResponse = {
+    topic: string;
+    data: object;
+    ts?: number;
+    type?: string;
+}
+
+// Subscription handlers
+const handleTradeSub = (response: WebSocketResponse) => {
+    
+}
+
+const handleLiquidationSub = (response: WebSocketResponse) => {
+    
+}
+
 // Connect
 const stream: WebSocket = new WebSocket(
   "wss://stream.bybit.com/v5/public/linear"
 );
 
+const ping = {
+    "req_id": "100001", 
+    "op": "ping"
+}
+
 // Subscribe
-/* stream.on("open", () => {
-    const subRequest = {
-      action: "SubAdd",
-      subs: ["publicTrade.BTCUSDT"],
-    };
-    stream.send(JSON.stringify(subRequest));
-});
- */
 stream.on("open", () => {
-    const subRequest = {
-      action: "SubAdd",
-      subs: ["liquidation.BTCUSDT"],
+    const message = {
+        "req_id": "subs",
+        "op": "subscribe",
+        // Subscriptions
+        "args": [
+            "liquidation.BTCUSDT",
+            "publicTrade.BTCUSDT",
+        ],
     };
-    stream.send(JSON.stringify(subRequest));
+    stream.send(JSON.stringify(message));
 });
 
 stream.on("message", (data) => {
-    console.log(data.toString());
+    const response: WebSocketResponse = JSON.parse(data.toString());
+    response.topic === "publicTrade.BTCUSDT" ? handleTradeSub(response) : handleLiquidationSub(response);
 });
+
+// Send ping keep alive every 20 secs
+setInterval(() => {
+    stream.send(JSON.stringify(ping));
+}, 20000)
+
